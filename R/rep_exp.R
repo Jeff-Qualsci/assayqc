f9293d1632e2a0b4590ef7d8afadee0017388b1f
Jeffrey Weidner <jeff@qualsci.com>
2025-08-11 18:42:59 -0400

    make_md_data() and associated tests completed

--- R/rep_exp.R
+++ R/rep_exp.R
@@ -1,2 +1,28 @@
-# Replicate-Experiment or Bland-Altman mean difference analysis.
-# Used for initial reproducibility assessment or to validate changes.
+make_md_data <- function(smpl = NULL, exp1, exp2, pot = TRUE) {
+  if (!is.numeric(exp1) | !is.numeric(exp2)) {
+    stop("Both exp1 and exp2 must be numeric vectors")
+  }
+  if (is.null(smpl)) {
+    smpl <- as.character(seq_along(exp1))
+    message("Sequential sample identifiers generated for each data pair")
+  }
+
+  if (!is.character(smpl)) {
+    stop("smpl must be a character vector")
+  }
+
+  if (!(length(smpl) == length(exp1) & (length(exp2) == length(exp1)))) {
+    stop("smpl, exp1, and exp2 must all be the same length")
+  }
+
+  if (pot) {
+    exp1 <- log10(exp1)
+    exp2 <- log10(exp2)
+  }
+
+  md_data <- tibble::tibble(smpl, meas1 = exp1, meas2 = exp2) |>
+    dplyr::mutate(
+      meas_mean = (meas1 + meas2) / 2,
+      meas_diff = meas2 - meas1
+    )
+}
--- tests/testthat/test-rep_exp.R
+++ tests/testthat/test-rep_exp.R
@@ -1,3 +1,32 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("make_md_data works.", {
+  exp1 <- c(2, 1, 0)
+  exp2 <- c(1, 1, 1)
+  smpl <- c("1", "2", "3")
+  meas_mean <- c(1.5, 1, 0.5)
+  meas_diff <- c(-1, 0, 1)
+  expect_md_data <- tibble::tibble(smpl, meas1 = exp1, meas2 = exp2, meas_mean, meas_diff)
+
+  # happy path
+  expect_equal(make_md_data(smpl = smpl, exp1 = exp1, exp2 = exp2, pot = FALSE), expect_md_data)
+
+  # exp1 and exp2 must both be numeric
+  expect_error(make_md_data(smpl, exp1, exp2 = smpl, pot = FALSE))
+
+  # generate smpl data and message if smpl is NULL
+  expect_message(make_md_data(exp1 = exp1, exp2 = exp2, pot = FALSE))
+
+  # confirm generated smpl is sequential character vector
+  expect_equal(make_md_data(exp1 = exp1, exp2 = exp2, pot = FALSE), expect_md_data)
+
+  # smpl must be a character vector
+  expect_error(make_md_data(smpl = TRUE, exp1 = exp1, exp2 = exp2, pot = FALSE))
+
+  # smpl, exp1, and exp2 must all be the same length
+  expect_error(make_md_data(smpl = smpl, exp1 = c(1:4), exp2 = exp2, pot = FALSE))
+
+  # confirm log10 transformation of data when pot = TRUE
+  expect_equal(
+    make_md_data(smpl = smpl, exp1 = c(100, 10, 1), exp2 = c(10, 10, 10), pot = TRUE),
+    expect_md_data
+  )
 })
